b3bf3c9e5eb5ea173411764c5b9d2b3912e3c0a2
BoghossianM <boghossian.matias@gmail.com>
2025-05-24 23:05:08 -0300

    Cambio1

--- public/includes/header.php
+++ public/includes/header.php
@@ -5,18 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="description" content="Servicios logísticos rápidos, seguros y eficientes." />
     <title>Logística PF</title>
-    <link rel="stylesheet" href="/css/style.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/login.css" />
+    <link rel="stylesheet" href="css/contact.css" />
 </head>
 <body>
 <header>
     <nav class="header-nav container">
-        <a href="/index.php" class="logo">Logística PF</a>
+        <a href="index.php" class="logo">Logística PF</a>
         <ul class="nav-list">
-            <li><a href="/servicios.php">Servicios</a></li>
-            <li><a href="/index.php#nosotros">Quiénes somos</a></li>
-            <li><a href="/contacto.php">Contacto</a></li>
-            <li><a href="/cotizar.php">Cotizar</a></li>
-            <li><a href="/login.php">Iniciar sesión</a></li>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="servicios.php">Servicios</a></li>
+            <li><a href="area.php">Área personal</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
         </ul>
     </nav>
 </header>
